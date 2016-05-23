8ccece3167049d8126afd8f275da7796075ed28e
liucheng <liucheng@sensetime.com>
2016-05-23 20:13:50 +0800

    fix all the compiling warnings and errors.

---
+++ fifo_32_256_inst.v
@@ -0,0 +1,10 @@
+fifo_32_256	fifo_32_256_inst (
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.rdreq ( rdreq_sig ),
+	.wrreq ( wrreq_sig ),
+	.almost_full ( almost_full_sig ),
+	.empty ( empty_sig ),
+	.full ( full_sig ),
+	.q ( q_sig )
+	);
